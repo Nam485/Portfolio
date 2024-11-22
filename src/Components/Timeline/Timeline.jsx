1d5ef372f7d7a4913f75c166c0b93a2877b268aa
Namit Goyal <99747345+Nam485@users.noreply.github.com>
2024-11-22 10:38:19 +0530

    Update Timeline.jsx

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -15,7 +15,7 @@ const Timeline = () => {
               <h3>CSE, B.Tech</h3>
               <h3>Birla Institute of Technology, Mesra</h3>
               <p>
-                I am currently pursuing a Bachelor's degree in Computer Science and Engineering at BIT, Mesra. I have completed 5 semesters and have a CGPA of 8.40. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.
+                I am pursuing a Bachelor's in Computer Science and Engineering at BIT, Mesra. I have completed 6 semesters and have a CGPA of 8.47. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.
               </p>
             </div>
             <div className="box">
@@ -23,7 +23,7 @@ const Timeline = () => {
               <h3>12th, RBSE</h3>
               <h3>Sarvodaya Sikshan Sansthan</h3>
               <p>
-                I completed my class 12th high school education at Sarvodaya Sikshan Sansthan. <br /> Score: 96.8%
+                I completed my 12th high school education class at Sarvodaya Sikshan Sansthan. <br /> Score: 96.8%
               </p>
             </div>
             <div className="box">
@@ -31,7 +31,7 @@ const Timeline = () => {
               <h3>10th, RBSE</h3>
               <h3>Maruti Inter School</h3>
               <p>
-                I completed my class 10th school education at Maruti Inter School. <br /> Score: 86.67%
+                I completed my 10th Secondary school education class at Maruti Inter School. <br /> Score: 86.67%
               </p>
             </div>
           </div>
@@ -58,7 +58,7 @@ const Timeline = () => {
               <h3>Haripratap Trust Memorial</h3>
               <p>
                 ◦ Crafted a website offering concise information about the NGO and facilitating volunteer connections. <br />
-                ◦ Contributed to the development of frontend components for the Haripratap Trust Memorial’s web applications. <br />
+                ◦ Contributed to developing frontend components for the Haripratap Trust Memorial’s web applications. <br />
                 ◦ Collaborated with the design and development teams to implement user interface enhancements tailored to the organization’s needs. <br />
                 ◦ Assisted in optimizing the frontend performance and user experience of the Trust’s digital platforms. <br />
                 Link: <a href="https://hariprataptrust.org.in" target="_blank" rel="noopener noreferrer">hariprataptrust.org.in</a>
